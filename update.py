960f8d464c50610ec0426da06afc117470abacb6
root <root@ip-172-31-6-70.ap-south-1.compute.internal>
2022-11-18 18:31:09 +0000

    changing image name

--- update.py
+++ update.py
@@ -6,14 +6,14 @@ import os
 import requests
 import sys
 
-def read_data():
-    inp = Path('Webserver-1/helm/values.yaml')
+def read_data(dir_name):
+    inp = Path('{}/helm/values.yaml'.format(dir_name))
     yaml = YAML()
     yaml.preserve_quotes = True
     yaml.default_flow_style=None
     with open(str(inp), "r") as yaml_file:
-        values_yml = yaml.load(yaml_file)  
-    return values_yml 
+        values_yml = yaml.load(yaml_file)
+    return values_yml
 
 
 def update_values(values_json):
@@ -33,9 +33,10 @@ def commit_to_git():
 
 
 if __name__ == '__main__':
-    image_name = sys.argv[1] 
+    image_name = sys.argv[1]
+    dir_name = sys.argv[2]
     repo = "https://github.com/arif11111/Webserver-hosting.git"
-    values_json = read_data()
+    values_json = read_data(dir_name)
     print(values_json)
     update_values(values_json)
     commit_to_git()
